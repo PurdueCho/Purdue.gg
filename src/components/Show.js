8bc4b5165e71f6683e6eee0e243fd55b7a4c24d0
Younghyun Cho <cho242@purdue.edu>
2018-12-06 21:47:33 -0500

    Error fix

--- src/components/Show.js
+++ src/components/Show.js
@@ -15,6 +15,10 @@ const styles = theme => ({
     width: '100%',
     backgroundColor: theme.palette.background.paper,
   },
+  chart: {
+    overflow: 'auto',
+    maxHeight: '70vh',
+  },
 });
 
 const course =['CS 30700','CS 31400','CS 33400','CS 34800','CS 35200','CS 35300','CS 35400','CS 35500','CS 37300','CS 38100','CS 39000 - VR','CS 39000 - WAP','CS 40700','CS 40800','CS 42200','CS 42600','CS 43400','CS 44800','CS 45600','CS 47100','CS 47300','CS 47800','CS 48300','CS 48900','CS 49000 - DSO','CS 49000 - SWS','CS 51400','CS 51500','STAT 41600','STAT 51200','MA 41600']
@@ -96,7 +100,7 @@ class Show extends React.Component {
         <Typography variant="title" gutterBottom align="center">
             PURDUE.GG
         </Typography>
-        <div>
+        <div className={classes.chart}>
              { this.state.courses ? this._renderStats() : 'Loading' }
         </div>
       </div>
